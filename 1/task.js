ac60e88ef237acffe4a8f9ae8c275c3acd1c7326
Stanciu Dragoș Ioan <stanciudragosioan92@gmail.com>
2023-12-18 19:36:56 +0200

    Update task.js

--- 1/task.js
+++ 1/task.js
@@ -1,1 +1,16 @@
 //write a function that returns the sum of 3 numbers
+function sumOfThreeNumbers(num1, num2, num3) {
+  if (
+    typeof num1 === "string" ||
+    typeof num2 === "string" ||
+    typeof num3 === "string"
+  ) {
+    console.log("please input numbers");
+    return;
+  }
+  return num1 + num2 + num3;
+}
+
+// Example usage:
+let result = sumOfThreeNumbers(2, 5, 7);
+console.log(result); // Output: 14
