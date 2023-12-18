3652748ec45adbba362ff204607516beb85a157c
DESKTOP-4CBAQT1\40729 <maftei.marius100@gmail.com>
2023-12-18 19:33:00 +0200

    exercise 1

--- 1/task.js
+++ 1/task.js
@@ -1,1 +1,13 @@
 //write a function that returns the sum of 3 numbers
+function sum (a, b, c) {
+
+    if (typeof a === "string" || typeof b === "string" || typeof c === "string") {
+        console.log ("Please input number");
+        return;
+    } 
+    const sum = a+b+c;
+    return sum;
+}
+
+console.log (sum(1, 2, "3"));
+
